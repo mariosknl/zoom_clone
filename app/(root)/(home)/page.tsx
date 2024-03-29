d81e099c1abf918421bae1c54a4cb65d19393261
mariosknl <mariosknl@outlook.com>
2024-03-29 17:41:51 +0200

    sidebar & navbar & mobileNav components created

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,8 @@
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<section className="flex size-full flex-col gap-10 text-white">
+			<h1 className="text-3xl font-bold">Home</h1>
+		</section>
+	);
 };
 export default Home;
---
+++ app/(root)/(home)/upcoming/page.tsx
@@ -0,0 +1,8 @@
+const Upcoming = () => {
+	return (
+		<section className="flex size-full flex-col gap-10 text-white">
+			<h1 className="text-3xl font-bold">Upcoming</h1>
+		</section>
+	);
+};
+export default Upcoming;
